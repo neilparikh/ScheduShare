a82272686a0b7944446ff49dbde095759dfcbce0
Neil Parikh <parikh.neil@me.com>
2014-09-20 04:46:05 -0400

    add redis

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
 require 'json'
+require 'redis'
+
+redis = Redis.new(:url => ENV["REDISTOGO_URL"])
+
+get '/set/:var' do
+  redis.set("test", params["var"])
+end
+
+get '/' do
+  redis.get("test")
+end
 
 get '/event/:id' do
   events = {"event_id" => params["id"], events: {"event1" => "12:30", "event2" => "12:45"}}
